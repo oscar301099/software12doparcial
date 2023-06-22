51e4e7c8ac2373aa85a30591b2a63d54b09cc259
oscar oros <oscarorosduran@gmail.com>
2023-06-22 14:06:44 -0400

    login

--- src/modules/auth/components/GoogleSignIn.tsx
+++ src/modules/auth/components/GoogleSignIn.tsx
@@ -1,26 +1,50 @@
-import {googleSignIn} from "@/modules/auth/services/auth";
-import {useRouter} from "next/navigation";
+import { googleSignIn } from "@/modules/auth/services/auth";
+import { useRouter } from "next/navigation";
 
 function GoogleSignIn() {
+  const router = useRouter();
 
-    const router = useRouter();
-    async function handleSignIn(e:any) {
-        try {
-            const authResponse = await googleSignIn();
-            if (authResponse){
-                router.push('/app');
-            }
-        }catch (e) {
-            console.log(e)
-            console.error(e);
-        }
-
+  async function handleSignIn(e: any) {
+    try {
+      const authResponse = await googleSignIn();
+      if (authResponse) {
+        router.push('/app');
+      }
+    } catch (e) {
+      console.log(e);
+      console.error(e);
     }
+  }
 
+  return (
+    <div className="bg-white rounded-md w-1/4 h-1/4 flex justify-center items-center">
+      <style>
+        {`
+          @keyframes animatedBackground {
+            0% { background-position: 0% 50%; }
+            50% { background-position: 100% 50%; }
+            100% { background-position: 0% 50%; }
+          }
 
-    return <div className={'bg-white rounded-md w-1/2 h-1/2 flex justify-center items-center'}>
-        <button className={'google-sign-in-btn'} onClick={handleSignIn}>{'Sign in with Google'}</button>
+          .bg-animated {
+            background: linear-gradient(45deg, #f3c5ad, #b4d8e7, #f3c5ad);
+            background-size: 200% 200%;
+            animation: animatedBackground 10s ease infinite;
+          }
+        `}
+      </style>
+      <div className="bg-animated p-4 rounded-lg">
+        Bienvenido al diagramador, por favor regístrate 
+        <button
+        className="google-sign-in-btn mt-4"
+        onClick={handleSignIn}
+      >
+        Google
+      </button>
+      </div>
+    
     </div>
+  );
 }
 
-export default GoogleSignIn
+export default GoogleSignIn;
